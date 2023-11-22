3e6631f584dae4779a5958b40125eed6ef1e8e83
Govie <127932645+ofcounsel@users.noreply.github.com>
2023-11-22 14:15:40 -0800

    Update gatsby-config.js
    changed ‘http://localhost:8000’ to ‘ https://passthebar.netlify.app’

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ try {
 
 if (
     process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8000` &&
+    config.siteUrl === `https://passthebar.netlify.app` &&
     !process.env.SITEURL
 ) {
     throw new Error(
